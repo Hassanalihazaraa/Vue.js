91a9ff8f2e636c7e6a8251eebe70bca150edefdc
Hassan Ahmed <hassan@Hassans-MacBook-Pro.local>
2020-12-31 15:12:38 +0100

    moved to component folder

---
+++ components/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <section>
+    <header>
+      <h1>My friends</h1>
+    </header>
+    <new-friend @add-contact="addContact"></new-friend>
+    <ul>
+      <friend-contact
+          v-for="friend in friends"
+          :key="friend.id"
+          :id="friend.id"
+          :name="friend.name"
+          :phone-number="friend.phone"
+          :email-address="friend.email"
+          :is-favourite="friend.isFavourite"
+          @toggle-favourite="toggleFavouriteStatus"
+          @delete="deleteContact"
+      ></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "Ali",
+          name: "Hassan",
+          phone: "0032484234234",
+          email: "hassan@gmail.com",
+          isFavourite: false,
+        },
+        {
+          id: "Hassan",
+          name: "Ahmed",
+          phone: "0032484234234",
+          email: "ahmed@gmail.com",
+          isFavourite: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleFavouriteStatus(friendId) {
+      const identifiedFriend = this.friends.find(friend => friend.id === friendId)
+      identifiedFriend.isFavourite = !identifiedFriend.isFavourite
+    },
+
+    addContact(name, phone, email) {
+      const newFriend = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavourite: false
+      }
+      this.friends.push(newFriend)
+    },
+
+    deleteContact(friendId) {
+      this.friends = this.friends.filter(friend => friend.id !== friendId)
+    }
+  }
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+</style>
---
+++ components/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,173 @@
+<template>
+  <li>
+    <h1>{{ name }} {{ isFavourite ? '(Favourite)' : '' }}</h1>
+    <button @click="toggleFavourite">Toggle Favourite</button>
+    <button @click="toggleDetails">
+      {{ isDetailVisible ? 'Hide' : 'Show' }} details
+    </button>
+    <ul v-if="isDetailVisible">
+      <li>{{ phoneNumber }}</li>
+      <li>{{ emailAddress }}</li>
+    </ul>
+    <button @click="$emit('delete', id)">Delete</button>
+  </li>
+</template>
+
+<script>
+export default {
+  /*props: [
+    'name',
+    'phoneNumber',
+    'emailAddress',
+    'isFavourite',
+  ],*/
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      /*validator: value => {
+        return value === '1' || value === '0'
+      }*/
+    }
+  },
+  emits: [
+    'toggle-favourite',
+    'delete',
+  ],
+  /*emits: {
+    'toggle-favourite': id => {
+      if (id) {
+        return true
+      } else {
+        console.log('id is missing!')
+        return false
+      }
+    }
+  },*/
+  data() {
+    return {
+      isDetailVisible: false,
+      /* friend: {
+         id: "Ali",
+         name: "Hassan",
+         phone: "0032484234234",
+         email: "hassan@gmail.com",
+       },*/
+      //IsFavouriteFriend: this.isFavourite
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.isDetailVisible = !this.isDetailVisible
+    },
+    // it is a custom event
+    toggleFavourite() {
+      this.$emit('toggle-favourite', this.id)
+    },
+    // you can creat custom events in methods or in the template
+    /*deleteFriend() {
+      this.$emit('delete-friend',)
+    }*/
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Jost', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li,
+#app form {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
+}
+</style>
