49afcd8e5f64f984dfb6824de0533570fd875df8
Hassan Ahmed <hassan@Hassans-MacBook-Pro.local>
2020-12-31 12:18:00 +0100

    installed vue cli and nodejs and build friend component

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <section>
+    <header>
+      <h1>My friends</h1>
+    </header>
+    <ul>
+      <friend-contact
+          v-for="friend in friends"
+          :key="friend.id"
+          :id="friend.id"
+          :name="friend.name"
+          :phone-number="friend.phone"
+          :email-address="friend.email"
+          :is-favourite="friend.isFavourite"
+          @toggle-favourite="toggleFavouriteStatus"
+      ></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "Ali",
+          name: "Hassan",
+          phone: "0032484234234",
+          email: "hassan@gmail.com",
+          isFavourite: false,
+        },
+        {
+          id: "Hassan",
+          name: "Ahmed",
+          phone: "0032484234234",
+          email: "ahmed@gmail.com",
+          isFavourite: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleFavouriteStatus(friendId) {
+      const identifiedFriend = this.friends.find(friend => friend.id === friendId)
+      identifiedFriend.isFavourite = !identifiedFriend.isFavourite
+    }
+  }
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+</style>
