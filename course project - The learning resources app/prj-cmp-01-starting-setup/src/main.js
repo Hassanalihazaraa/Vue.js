db252714f7d1d3c00f7b89e3dadc895dd9d3d1b9
Hassan Ahmed <hassan@Hassans-MacBook-Pro.local>
2021-01-03 18:04:44 +0100

    follow up

--- course project - The learning resources app/prj-cmp-01-starting-setup/src/main.js
+++ course project - The learning resources app/prj-cmp-01-starting-setup/src/main.js
@@ -1,3 +1,15 @@
-import { createApp } from 'vue';
+import {createApp} from 'vue';
+import App from "@/App";
 
-createApp({}).mount('#app');
+// import components
+import BaseCard from '@/components/UI/BaseCard'
+import BaseButton from "@/components/UI/BaseButton";
+
+const app = createApp(App)
+
+// register components
+app.component('base-card', BaseCard)
+app.component('base-button', BaseButton)
+
+
+app.mount('#app');
