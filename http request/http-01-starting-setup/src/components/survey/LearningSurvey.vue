b557a4fd5eb1e6d14c3bbbc00556d2429715acf3
Hassan Ahmed <hassan@Hassans-MacBook-Pro.local>
2021-01-04 12:21:32 +0100

    http request done

--- http request/http-01-starting-setup/src/components/survey/LearningSurvey.vue
+++ http request/http-01-starting-setup/src/components/survey/LearningSurvey.vue
@@ -5,30 +5,31 @@
       <form @submit.prevent="submitSurvey">
         <div class="form-control">
           <label for="name">Your Name</label>
-          <input type="text" id="name" name="name" v-model.trim="enteredName" />
+          <input type="text" id="name" name="name" v-model.trim="enteredName"/>
         </div>
         <h3>My learning experience was ...</h3>
         <div class="form-control">
-          <input type="radio" id="rating-poor" value="poor" name="rating" v-model="chosenRating" />
+          <input type="radio" id="rating-poor" value="poor" name="rating" v-model="chosenRating"/>
           <label for="rating-poor">Poor</label>
         </div>
         <div class="form-control">
           <input
-            type="radio"
-            id="rating-average"
-            value="average"
-            name="rating"
-            v-model="chosenRating"
+              type="radio"
+              id="rating-average"
+              value="average"
+              name="rating"
+              v-model="chosenRating"
           />
           <label for="rating-average">Average</label>
         </div>
         <div class="form-control">
-          <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating" />
+          <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating"/>
           <label for="rating-great">Great</label>
         </div>
         <p
-          v-if="invalidInput"
+            v-if="invalidInput"
         >One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="error"> {{ error }}</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -44,9 +45,10 @@ export default {
       enteredName: '',
       chosenRating: null,
       invalidInput: false,
+      error: null
     };
   },
-  emits: ['survey-submit'],
+  //emits: ['survey-submit'],
   methods: {
     submitSurvey() {
       if (this.enteredName === '' || !this.chosenRating) {
@@ -55,10 +57,33 @@ export default {
       }
       this.invalidInput = false;
 
-      this.$emit('survey-submit', {
+      /*this.$emit('survey-submit', {
         userName: this.enteredName,
         rating: this.chosenRating,
-      });
+      });*/
+
+      this.error = null
+      fetch('https://vue-http-demo-98936-default-rtdb.europe-west1.firebasedatabase.app/surveys.json', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: this.enteredName,
+          rating: this.chosenRating
+        })
+      })
+          .then(response => {
+            if (response.ok) {
+              //dfdf
+            } else {
+              throw new Error('could not save data')
+            }
+          })
+          .catch(error => {
+            console.log(error)
+            this.error = error.message
+          })
 
       this.enteredName = '';
       this.chosenRating = null;
