6816768e8d2d7fef613028e73bdebe89e59b683d
Hassan Ahmed <hassan@Hassans-MacBook-Pro.local>
2021-01-04 12:32:40 +0100

    routing

---
+++ routing/routing-01-starting-setup/src/main.js
@@ -0,0 +1,13 @@
+import {createApp} from 'vue';
+import {createRouter} from "vue-router"
+
+import App from './App.vue';
+
+const router = createRouter({
+    
+})
+
+
+const app = createApp(App)
+
+app.mount('#app');
